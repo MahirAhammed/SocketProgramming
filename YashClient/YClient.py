879b5f551dcadc870824f084bae72d1e71fb6ee7
Yramklass <yramklass@gmail.com>
2024-02-26 12:04:29 +0200

    Tested & Debugged login and get status

--- YashClient/YClient.py
+++ YashClient/YClient.py
@@ -13,20 +13,20 @@ def clear_screen():
 
 
 
-serverName = ""
+serverName = "192.168.56.1"
 serverPort = 12001
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
 
 while logged_in == False:
-    username = raw_input("Enter Username:\n")
-    password = raw_input("Enter Password:\n")
-    message = "LOGIN " + "\r\n" + "USERNAME " + username.upper() + "\r\nPASSWORD " + password.upper() + "\r\nIP NUMBER " + clientSocket.getsockname + "\r\n\r\n"
+    username = input("Enter Username:\n")
+    password = input("Enter Password:\n")
+    message = "LOGIN " + "\r\n" + "USERNAME " + username + "\r\nPASSWORD " + password + "\r\nIP NUMBER " + clientSocket.getsockname()[0] + "\r\n\r\n"
     clientSocket.send(message.encode())
-    returnmessage = clientSocket.recv(1024)
+    returnmessage = clientSocket.recv(1024).decode()
     #Decision Tree
-    returncommand = returnmessage[0:returnmessage.index("\r")]
-    reason = returnmessage[returnmessage.index("\n")+1:]
+    returncommand = returnmessage[:returnmessage.find("\r")-1]
+    reason = returnmessage[returnmessage.find("\n")+1:-5]
 
     if returncommand == "UNSUCCESSFUL":
         if reason == "DUPLICATE":                                                       # If user logged in already
@@ -40,21 +40,22 @@ while logged_in == False:
         else:
             print ("Welcome back {}!".format(username))
         logged_in = True
-    
+
+print(logged_in)  
    
    
 
 
 while logged_in == True:
     
-    message = "STATUS \r\nGET\r\n" + "\r\n" + "USERNAME " + username 
+    message = "STATUS \r\nGET\r\n" + "USERNAME " + username +"\r\n\r\n"
     clientSocket.send(message.encode())
-    returnmessage = clientSocket.recv(1024)
-    print ("Your status is: " + returnmessage[returnmessage.index("\n")+1:]) #Protocol : "STATUS \r\n userstatus\r\n\r\n"
+    returnmessage = clientSocket.recv(1024).decode()
+    print ("Your status is: " + returnmessage[returnmessage.find("\n")+1:]) #Protocol : "STATUS \r\n userstatus\r\n\r\n"
     
-    options = "Choose an option:\n1.) \n2.) \n3.) \n4.) \n5.)"              #String of options to be displayed
+    options = "Choose an option:\n1.) \n2.) \n3.) \n4.) \n5.)\n"              #String of options to be displayed
 
-    user_choice = (raw_input(options)).upper() #add all options
+    user_choice = (input(options))#add all options
 
     if user_choice == "":                                           #Should be second last option
         clientSocket.close()
@@ -64,7 +65,7 @@ while logged_in == True:
         exit
 
     if user_choice == "S":
-        newstatus = raw_input("What would you like to set your status to?\n1.) Available\n2.) Away\n")
+        newstatus = input("What would you like to set your status to?\n1.) Available\n2.) Away\n")
 
         if newstatus == "1":
             message = "STATUS \r\nSET\r\n" + "USERNAME {}\r\n".format(username) +  "AVAILABLE\r\n\r\n"
--- server.py
+++ server.py
@@ -1,44 +1,49 @@
 from socket import *
 from user import *
-
 users = []
-PORT=12001
-HOST = ''
 
-server = socket(AF_INET,SOCK_STREAM)
-server.bind((HOST,PORT))
+def main():
+    PORT=12001
+    HOST = "192.168.56.1"
 
-server.listen(10)
-print("Waiting for connection...")
+    server = socket(AF_INET,SOCK_STREAM)
+    server.bind((HOST,PORT))
 
-while True:
-    response =""
+    server.listen(10)
+    print("Waiting for connection...")
     connectionSocket, addr = server.accept()
     print(f"Connected to {addr}")
 
-    #Receive message from client
-    message = connectionSocket.recv(1024).decode()
-    print(message)
+    while True:
+        #Receive message from client
+        message = connectionSocket.recv(1024).decode()
+        print(message)
 
-    #Decision tree // formulate response depending on message received
-    command = message[0:message.index("\r")]
-    message = message [message.index("\n") + 1:]
-    if command == "LOGIN":
-        login(message)
-    if command == "STATUS":
-        status(message)
+        #Decision tree // formulate response depending on message received
+        command = message[0:message.find("\r")-1]
+        message = message [message.find("\n") + 1:]
+        if command == "LOGIN":
+            returnmessage = login(message)
 
-    #Send response to client
-    connectionSocket.send(response.encode())
-    connectionSocket.close()
+        if command == "STATUS":
+            returnmessage = status(message)
+
+        if command == "DISCONNECT":
+            connectionSocket.close()
+
+        #Send response to client
+        print (returnmessage)
+        connectionSocket.send(returnmessage.encode())
+
+        
 
 
 def login(message):
-    username = message[10:message.index("\r")]      #LOGIN PROTOCOL: "LOGIN " + "\r\n" + "USERNAME " + username.upper() + "\r\nPASSWORD " + password.upper() + "\r\nIP NUMBER " + clientSocket.getsockname + "\r\n\r\n"
-    message = message [message.index("\n")+1:]
-    password = messasge[10:message.index("\r")]
-    message = message [message.index("\n")+1:]
-    ip_num = message[11:message.index("\r")]
+    username = message[9:message.find("\r")]      #LOGIN PROTOCOL: "LOGIN " + "\r\n" + "USERNAME " + username + "\r\nPASSWORD " + password+ "\r\nIP NUMBER " + clientSocket.getsockname + "\r\n\r\n"
+    message = message[message.find("\n")+1:]
+    password = message[9:message.find("\r")]
+    message = message[message.find("\n")+1:]
+    ip_num = message[10:message.find("\r")]
     found = False
     for x in users:
         if x.get_username == username:          #RESPONSE PROTOCOL: "SUCCESSFUL/UNSUCCESSFUL \r\n" + "REASON \r\n\r\n"
@@ -54,34 +59,38 @@ def login(message):
                     x.set_ip_num(ip_num)
             else:
                 response =  "UNSUCCESSFUL \r\n" + "PASSWORD \r\n\r\n"
-        if found == False :
-            users.append(user(username,password,ip_num,"AVAILABLE"))
-            response = "SUCCESSFUL \r\n" + "NEW \r\n\r\n"
+    if found == False :
+        users.append(user(username,password,ip_num,"AVAILABLE"))
+        response = "SUCCESSFUL \r\n" + "NEW \r\n\r\n"
+    return response
 
 #Get a user's status or set new status
-def status(message):                                # Get Protocol: "STATUS \r\nGET" + "\r\n" + "USERNAME " + username 
-    commanddetails = message[:message.index("\r")]
-    message = message[message.index("\n")+1:]
-    username = message[10:message.index("\r")]
-    message = message[message.index("\n")+1:]
+def status(message):                                # Get Protocol: "STATUS \r\nGET\r\n" + "USERNAME " + username +"\r\n\r\n"
+    commanddetails = message[:message.find("\r")]
+    message = message[message.find("\n")+1:]
+    username = message[9:message.find("\r")]
+    
     if commanddetails == "GET":
         userstatus = ""
         for x in users:
-            if x.get_username == username:
+            if x.get_username() == username:
                 userstatus = x.get_status()
         if userstatus == "":
             response = "STATUS \r\nDNE\r\n\r\n"
         else:
             response = "STATUS \r\n{}\r\n\r\n".format(userstatus)
     else:                                             # Set Protocol: "STATUS \r\n" + "SET\r\n" + "USERNAME {}\r\n".format(username) + "AVAILABLE\r\n\r\n"
-        newstatus = message[:message.index("\r")]
+        message = message[message.find("\n")+1:]
+        newstatus = message[:message.find("\r")]
         for x in users:
             if x.get_username==username:
                 x.set_status(newstatus)
-                
+    return response
         
 
-     
+
+if __name__ == "__main__":
+    main()
     
 
      
