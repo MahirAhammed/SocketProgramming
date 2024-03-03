032e756741a062afbd39958d5ebfe82765be616e
Mahir Ahammed <itsmahir24@gmail.com>
2024-03-03 13:53:57 +0200

    Client interactions updated and login validation added

--- Mahir/client.py
+++ Mahir/client.py
@@ -4,99 +4,115 @@ from threading import Thread
 
 serverName = "localhost"             # UCT : "196.47.229.247"
 serverPort = 12001
-clientSocket = socket(AF_INET, SOCK_STREAM)       #Socket for TCP connection with the server
-clientSocket.connect((serverName,serverPort))
+
+serverSocket = socket(AF_INET, SOCK_STREAM)       #Socket for TCP connection with the server
+serverSocket.connect((serverName,serverPort))
 
 chatSocket = socket(AF_INET, SOCK_DGRAM)          #Socket for UDP connection with other clients
-chatSocket.bind((clientSocket.getsockname()[0],0))  #Default port generated
+chatSocket.bind((serverSocket.getsockname()[0],0))  #Default port generated for receiving chats
 
 username=""
 
 def main():
     logged_in = False
+    print("==============WELCOME==============")
+    print("\nEnter QUIT to leave the login screen")
+    #Login to server
     while logged_in == False:
         global username
         username = input("Enter Username:\n")
+
+        if (username == 'QUIT'):
+            break
+
+        if validateUsername(username) == False:  # checks whether username is valid
+            continue
+
         password = input("Enter Password:\n")
-        message = "LOGIN " + "\r\n" + "USERNAME " + username + "\r\nPASSWORD " + password+ "\r\nIP NUMBER " + clientSocket.getsockname()[0] + "\r\nSOCKET NUMBER " + str(chatSocket.getsockname()[1]) + "\r\n\r\n"
+        if (password == 'QUIT'):
+            break
 
-        clientSocket.send(message.encode())                                                         #Send login request with above details 
-        returnmessage = clientSocket.recv(1024).decode()
-        returncommand = returnmessage[:returnmessage.find("\r")-1]
-        reason = returnmessage[returnmessage.find("\n")+1:-5]                                       
+        message = "LOGIN " + "\r\n" + "USERNAME " + username + "\r\nPASSWORD " + password+ "\r\nIP NUMBER " + serverSocket.getsockname()[0] + "\r\nSOCKET NUMBER " + str(chatSocket.getsockname()[1]) + "\r\n\r\n"
 
-        if returncommand == "UNSUCCESSFUL":
-            if reason == "DUPLICATE":                                                               # If user logged in already
-                print("\nUser ({}) already logged into the server.\n".format(username))
-            else:                                                                                   # If incorrect password
-                print("\nPassword incorrect.\n")
+        serverSocket.send(message.encode())            #Send login request to server
+        response = serverSocket.recv(1024).decode()     # response from server regarding login
+
+        feedback = response[:response.find("\r")-1]     #Extract server feedback to determine what action to take
+        reason = response[response.find("\n")+1:-5]      # Extract the reason for above feedback                                 
+
+        if feedback == "UNSUCCESSFUL":
+            if reason == "DUPLICATE":                                                   # If user logged in already
+                print(f"\nYou are already logged into the server.\n")
+            else:                                                                       # If incorrect password
+                print("\nIncorrect password.\n")
             
-        else:                                                                                       # Successful login
-            if reason == "NEW":                                                                     #New user
-                print ("\nNew user successfully registered.\n")
-            else:                                                                                   #Existing user
-                print ("\nWelcome back {}!\n".format(username))
+        else:                                                               # Successful login
+            if reason == "NEW":                                             # New user
+                print ("\nYou are successfully registered.\n")
+            else:                                                           # Existing user
+                print (f"\nWelcome back {username}!\n")
             logged_in = True
 
 
-
+    # Main menu
     while logged_in == True:
         
-        print("Current User: {}\n".format(username))
-        message = "GETSTATUS \r\n" + "USERNAME " + username +"\r\n\r\n"
-        clientSocket.send(message.encode())
-        returnmessage = clientSocket.recv(1024).decode()
-        print ("Current Status: " + returnmessage[returnmessage.find("\n")+1:])                     #Protocol : "STATUS \r\n userstatus\r\n\r\n"
+        message = "GETSTATUS \r\n" + "USERNAME " + username +"\r\n\r\n"  # Protocol to get current status of user
+        serverSocket.send(message.encode())                              # Request to server
+        response = serverSocket.recv(1024).decode()                     #Response from server
+        print ("Your Status: " + response[response.find("\n")+1:])     #Protocol : "STATUS \r\n userstatus\r\n\r\n"
         
-    
-        options = "Choose an option:\n1.) Chat\n2.) List Clients\n3.) Set Status\n4.) Exit\n"       #String of options to be displayed
+        prompt = "Choose an option:\n[1] Chat\n[2] Show Clients List\n[3] Set Status\n[4] Log out\n"       #String of options to be displayed
  
-        user_choice = (input(options))
+        user_choice = (input(prompt))
 
-        if user_choice == "1":                                                                      #Chat with a specified user (they will not receive messages unless they choose to chat with you as well)
+        if user_choice == "1":                    
+             # Chat with a specified user (they will not receive messages unless they choose to chat with you as well)
             global peer_username
             peer_username = input("Enter Peer Username:\n")
             print("\n")
             chat(peer_username,username)            
 
-        elif user_choice == "2":                                                                    #List clients
-            message = "LIST \r\n"
-            clientSocket.send(message.encode())
-            returnmessage = clientSocket.recv(1024).decode()                                        #RETURN PROTOCOL: "LIST \r\n" + "username\rstatus\r\n" + ....
-            returnmessage = returnmessage[returnmessage.find("\n")+1:]
-            client_list = "\tLIST OF USERS:\t\nUSERNAME\t"+ "STATUS".ljust(10) + "\t\n"             #Will only list usernames and statuses of Available, Busy (chatting) and Offline users
-            while (returnmessage!="\r\n\r\n"):                                                      #while not end of the message / list
-                client_list += (returnmessage[:returnmessage.find("\r")]).ljust(10)
-                returnmessage = returnmessage[returnmessage.find("\r")+1:]
+        elif user_choice == "2":                            # List clients
+            message = "LIST \r\n"                           # Send request to server
+            serverSocket.send(message.encode())             # server reponse
+            response = serverSocket.recv(1024).decode()                                        #RETURN PROTOCOL: "LIST \r\n" + "username\rstatus\r\n" + ....
+            response = response[response.find("\n")+1:]
+            client_list = "\tLIST OF USERS:\t\nUSERNAME\t\t"+ "STATUS".ljust(20) + "\t\n"             # list all users and their statuses except for users with status set as AWAY
+            client_list +='_________________________________________________\n'
 
-                client_list += "\t{}\n".format((returnmessage[:returnmessage.find("\r")]).ljust(10))
-                returnmessage = returnmessage[returnmessage.find("\n")+1:]
+            while (response!="\r\n\r\n"):                               #while not end of the message / list
+                # Extracts username as formats to left for displaying purposes
+                client_list += (response[:response.find("\r")]).ljust(20)
+                response = response[response.find("\r")+1:]
 
-                
+                # Extracts user statuses and formats it for displaying purposes
+                client_list += "\t{}\n".format((response[:response.find("\r")]).ljust(20))
+                response = response[response.find("\n")+1:]
+
             print(client_list)
-        
 
-     
             
-        elif user_choice == "3":                                                                                #Set status to Available or Away (other statuses cannot be set by user [BUSY(chatting) and OFFLINE])
-            newstatus = input("What would you like to set your status to?\n1.) Available\n2.) Away\n")
+        elif user_choice == "3":     # Set status to Available or Away (other statuses cannot be set by user [BUSY(chatting) and OFFLINE])
+            newstatus = input("Set your status?\n[1] Available\n[2] Away\n")
             if newstatus == "1":
                 message = "SETSTATUS \r\n" + "USERNAME {}\r\n".format(username) +  "AVAILABLE\r\n\r\n"
-                clientSocket.send(message.encode())
-                returnmessage = clientSocket.recv(1024).decode()                                                #return message is just "DONE" (to ensure synchronization) as status is shown each time the menu is printed
+                serverSocket.send(message.encode())
+                response = serverSocket.recv(1024).decode()      #response is just "DONE" (to ensure synchronization) as status is shown each time the menu is printed
             elif newstatus == "2":
                 message = "SETSTATUS \r\n" + "USERNAME {}\r\n".format(username) + "AWAY\r\n\r\n"
-                clientSocket.send(message.encode())
-                returnmessage = clientSocket.recv(1024).decode()
+                serverSocket.send(message.encode())
+                response = serverSocket.recv(1024).decode()
             else:
                 print("Invalid choice.\n")
 
         
         
-        elif user_choice == "4":                                                                                #Disconnect from server and close client program
+        elif user_choice == "4":                 # Disconnect from server and close client program
             message = message = "SETSTATUS \r\n" + "USERNAME {}\r\n".format(username) +  "OFFLINE\r\n\r\n"
-            clientSocket.send(message.encode())
-            clientSocket.close()
+            serverSocket.send(message.encode())
+            serverSocket.close()
+            print("Logging out....")
             exit()
 
 
@@ -113,12 +129,12 @@ def recv_messages():
                 connected=True
                 global username
                 message = "SETSTATUS \r\n" + "USERNAME {}\r\n".format(username) +  "BUSY\r\n\r\n"   
-                clientSocket.send(message.encode())
-                returnmessage = clientSocket.recv(1024).decode()
+                serverSocket.send(message.encode())
+                returnmessage = serverSocket.recv(1024).decode()
     
                 
-            if data == 'QUIT':                                                         #In order to leave a chat, user types "QUIT_CHAT" - here it is received from the peer
-                                                                                            #Global variable set
+            if data == 'QUIT':            #In order to leave a chat, user types "QUIT" - here it is received from the peer
+            #Global variable set
                 print("{} has left the chat. Press enter to continue...".format(peer_username))
                 chatSocket.close()
                 return
@@ -156,8 +172,8 @@ def chat_session(peer_ip,peer_port):
 
 def chat(peer_username,username):
     message = "CHAT \r\nSTART\r\n{}\r\n\r\n".format(peer_username)           #Message to server requesting chat information
-    clientSocket.send(message.encode())
-    returnmessage = clientSocket.recv(1024).decode()                                          #Chat info - peer status + other info if peer is available  
+    serverSocket.send(message.encode())
+    returnmessage = serverSocket.recv(1024).decode()                                          #Chat info - peer status + other info if peer is available  
     command = returnmessage[:returnmessage.find("\r")]
 
     if command == "BUSY":
@@ -185,17 +201,28 @@ def chat(peer_username,username):
         
 
         message = "SETSTATUS \r\n" + "USERNAME {}\r\n".format(username) +  "AVAILABLE\r\n\r\n"              #Set user status back to Available once they have finished chatting
-        clientSocket.send(message.encode())
-        returnmessage = clientSocket.recv(1024).decode()
+        serverSocket.send(message.encode())
+        returnmessage = serverSocket.recv(1024).decode()
         global chatSocket
         chatSocket = socket(AF_INET, SOCK_DGRAM)   
-        chatSocket.bind((clientSocket.getsockname()[0],0))
+        chatSocket.bind((serverSocket.getsockname()[0],0))
         message = "SOCKET \r\n{}\r\n{}\r\n\r\n".format(username,chatSocket.getsockname()[1])
-        clientSocket.send(message.encode())
-        returnmessage = clientSocket.recv(1024).decode()
+        serverSocket.send(message.encode())
+        returnmessage = serverSocket.recv(1024).decode()
 
-        
-            
+def validateUsername(username):
+    
+    invalid_Names = ["LOGIN", "GETSTATUS", "SETSTATUS", "SOCKET", "UDP", "TCP", "AVAILABLE", "BUSY", "OFFLINE", "ONLINE","DNE"]
+
+    if username.strip().upper() in invalid_Names:     # if username any of the invalid names in the list
+        print("========!!! Invalid username, username clashes with protocols !!!==========")
+        return False
+    
+    elif username.isalnum() == False:  # if username contains any character beside numbers and letters
+        print("=======!!! Username cannot contain special characters including space !!!=======")
+        return False
+
+    return True    
             
 if __name__ == "__main__":
     main()
